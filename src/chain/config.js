0ce43fe8e07090ad2c1bcf60a36a0d3265681431
zhang <zhanghuaqiang@gmail.com>
2020-08-26 11:53:08 +0800

    change config for aggron test net

--- src/chain/config.js
+++ src/chain/config.js
@@ -1,8 +1,8 @@
 export const CELLAPI_URL = 'https://cellapitset.ckb.pw'
 export const CKB_NODE_URL = 'https://aggron.ckb.dev';
 
-const EOS_ARG = 0x0f9f2769fd63d1c7bf6447dc9f2b4cfbc7daf1e17be427674247c95e70e01379
-const EOS_TXHASH = 0x591a59c053ac1737f190f985329ca3213dde5fb9376c6107fac9e445434da4ca
+const EOS_ARG = 0xf42d2fbf79729f70098d60d014c9ab5c6901bbe3dadb45e681c3f0a55ed56746
+const EOS_TXHASH = 0xca7549786315cf47453dc5cb203e99bfd4aabe2bfc9e6f34601ccb0940a8c72b
 
 const TRON_ARG = 0x0f9f2769fd63d1c7bf6447dc9f2b4cfbc7daf1e17be427674247c95e70e01379
 const TRON_TXHASH = 0x591a59c053ac1737f190f985329ca3213dde5fb9376c6107fac9e445434da4ca
