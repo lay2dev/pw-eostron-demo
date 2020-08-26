64efb005a346ec85d83df3e9914543e0375046b1
zhang <zhanghuaqiang@gmail.com>
2020-08-26 11:46:54 +0800

    change config for aggron test net

--- src/chain/config.js
+++ src/chain/config.js
@@ -1,11 +1,21 @@
+export const CELLAPI_URL = 'https://cellapitset.ckb.pw'
+export const CKB_NODE_URL = 'https://aggron.ckb.dev';
+
+const EOS_ARG = 0x0f9f2769fd63d1c7bf6447dc9f2b4cfbc7daf1e17be427674247c95e70e01379
+const EOS_TXHASH = 0x591a59c053ac1737f190f985329ca3213dde5fb9376c6107fac9e445434da4ca
+
+const TRON_ARG = 0x0f9f2769fd63d1c7bf6447dc9f2b4cfbc7daf1e17be427674247c95e70e01379
+const TRON_TXHASH = 0x591a59c053ac1737f190f985329ca3213dde5fb9376c6107fac9e445434da4ca
+
+
 export const eosLockCell = {
   type: {
     codeHash: '0x00000000000000000000000000000000000000000000000000545950455f4944',
     hashType: 'type',
-    args: '0x3768ddcf394f6e94fc2543f7cd2317f28a8ab86deab86f23d87d39c42244b006'
+    args: EOS_ARG
   },
   outPoint: {
-    txHash: '0xd4763edfa494fa8823d765eb481fbd6246d38ad85426163f9aec43678ba7a289',
+    txHash: EOS_TXHASH,
     index: '0x0',
   },
 };
@@ -14,10 +24,10 @@ export const tronLockCell = {
   type: {
     codeHash: '0x00000000000000000000000000000000000000000000000000545950455f4944',
     hashType: 'type',
-    args: '0x246a96dcf87c03fbd50c5b9cce01af72b1571c1f1a889a1e6e6a4c5879c59bc0'
+    args: TRON_ARG,
   },
   outPoint: {
-    txHash: '0x117ff6114a75f78533b59a554de83c8c4e03e9a121eebd67d93c4ff16f6f2721',
+    txHash: TRON_TXHASH,
     index: '0x0',
   },
 
@@ -25,5 +35,3 @@ export const tronLockCell = {
 
 export const blockAssemblerCode = '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8';
 export const multiSigTypeId = '0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8';
-export const CELLAPI_URL = 'http://192.168.2.150:3000';
-export const CKB_NODE_URL = 'http://192.168.2.150:8116';
